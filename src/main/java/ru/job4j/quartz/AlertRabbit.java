25c4e72584e1735b6713e0b4841103c29bdd0f6d
KirAlex008 <kirshaav@yandex.ru>
2020-10-02 12:22:43 +0300

    1. Quartz [#299865]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,39 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    public static void main(String[] args) {
+        FileConfinReader reader = new FileConfinReader("C:/projects/job4j_grabber/src/main/resources/rabbit.properties");
+        reader.load();
+        int timeInterval = Integer.parseInt(reader.value("rabbit.interval"));
+
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(timeInterval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/quartz/FileConfinReader.java
@@ -0,0 +1,29 @@
+package ru.job4j.quartz;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+public class FileConfinReader {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public FileConfinReader(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().filter(line -> line.contains("="))
+                    .map(s -> s.split("=", 2))
+                    .forEach(s -> values.put(s[0],s[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+}
