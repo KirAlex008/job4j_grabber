6fde31913b631f0ad24236458edaca486298abeb
KirAlex008 <kirshaav@yandex.ru>
2020-10-07 09:46:36 +0300

    2. Парсинг HTML страницы. [#299867] error correction

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -10,17 +10,17 @@ public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements rows = doc.select(".postslisttopic");
-       /* for (Element row : rows) {
+        for (Element row : rows) {
             Element href = row.child(0);
             //System.out.println(href.attr("href"));
             //System.out.println(href.text());
             Element date = row.parent().child(5);
             System.out.println(date);
-        }*/
+        }
 
         Elements rowData = doc.select(".altCol");
         for (Element elem : rowData) {
-            String date = elem.getElementsMatchingText("\\d{2}\\s[а-яА-ЯёЁ]{3}\\s\\d{2},\\s\\d{2}:\\d{2}").text();
+            String date = elem.getElementsMatchingText("[а-яА-ЯёЁ]{5,},\\s\\d{2}:\\d{2}|\\d{1,}\\s[а-яА-ЯёЁ]{3}\\s\\d{2},\\s\\d{2}:\\d{2}").text();
             System.out.println(date);
         }
 
--- src/main/java/ru/job4j/html/parsExp.java
+++ src/main/java/ru/job4j/html/parsExp.java
@@ -12,9 +12,10 @@ import java.util.regex.Pattern;
 
 public class parsExp {
     public static void main(String[] args) throws Exception {
-        //15 сен 20, 14:09
-    Pattern pattern = Pattern.compile("\\d{2}\\s[а-яА-ЯёЁ]{3}\\s\\d{2},\\s\\d{2}:\\d{2}");
-    String input = "20 сен 20, 14:09";
+        //15 сен 20, 14:09 "\\d{2}\\s[а-яА-ЯёЁ]{3}\\s\\d{2},\\s\\d{2}:\\d{2}"
+        //сегодня, 05:09
+    Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{5,},\\s\\d{2}:\\d{2}|\\d{2}\\s[а-яА-ЯёЁ]{3}\\s\\d{2},\\s\\d{2}:\\d{2}");
+    String input = "сегодня, 05:09";
     Matcher matcher = pattern.matcher(input);
     boolean found = matcher.find();
         if(found)
