7a2a3d14480f0d88d789be541209feac60ae18f9
KirAlex008 <kirshaav@yandex.ru>
2020-10-30 09:57:37 +0300

    3. Шаблонизатор. [#299968]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+    @Test
+    public void whenWork() {
+        Generator generator = new Generator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        String rsl = generator.produce(template, args);
+        assertThat(rsl, is("I am a Petr Arsentev, Who are you? "));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNotWork() {
+        Generator generator = new Generator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("object", "car");
+        String rsl = generator.produce(template, args);
+    }
+}
